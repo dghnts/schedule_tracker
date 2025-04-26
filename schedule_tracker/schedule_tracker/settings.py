b44c051f98a9938c950ed2fba310d5c46991ae3c
KUNIMITSU ASANO <mittsulann73@gmail.com>
2025-04-26 12:33:22 +0900

    不要ファイルの削除

--- schedule_tracker/schedule_tracker/settings.py
+++ schedule_tracker/schedule_tracker/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'tracker.TrackerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tasks.apps.TasksConfig',
+    'schedules.apps.SchedulesConfig',
 ]
 
 MIDDLEWARE = [
