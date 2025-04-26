c8e54be0413f4a80c353b51629fe617edf5d8c7a
KUNIMITSU ASANO <mittsulann73@gmail.com>
2025-04-26 12:59:18 +0900

    task一覧ページへの繊維処理の作成

--- schedule_tracker/tasks/views.py
+++ schedule_tracker/tasks/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from django.views import View
+
 # Create your views here.
+class TaskIndexView(View):
+    def get(self, request, *args):
+        # タスク一覧ページを表示する
+        return render(request,"tasks/index.html")
+
+task_index_view = TaskIndexView.as_view()
